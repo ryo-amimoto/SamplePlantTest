a9708690f700579cbf55fe4d7c7ef139198258de
ryo-amimoto <amimoto@cdl-net.com>
2020-10-20 17:44:18 +0900

    マクロ機能を省略 2010/10/20

--- runPlantTest.m
+++ runPlantTest.m
@@ -10,28 +10,30 @@ InputHead = num2cell(importdata("InputIF_In_Header.xlsx"));
 OutputHead = num2cell(importdata("InputIF_Out_Header.xlsx"));
 InputData = vertcat(InputHead,InputVal);
 OutputData = vertcat(OutputHead,OutputVal);
-xlswrite("TestGraphOutput.xlsm",InputVal,"InputIF_In",'A2:J412');      % モデルの入力データを”テストファイル＿マクロ機能付き”へ出力
-xlswrite("TestGraphOutput.xlsm",OutputVal,"InputIF_Out",'A2:I412');    % モデルの出力データを”テストファイル＿マクロ機能付き”へ出力
+
+% モデルの入出力データを”テストファイル＿マクロ機能付き”へ出力
+xlswrite("TestGraphOutput.xlsm",InputVal,"InputIF_In",'A2:J412');
+xlswrite("TestGraphOutput.xlsm",OutputVal,"InputIF_Out",'A2:I412');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % テストデータのグラフ出力をエクセルのマクロ機能で実行　   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% オブジェクトの生成 
-ExcelApp = actxserver('Excel.Application'); 
-% Excel画面の表示 
-ExcelApp.Visible = 1; 
-% カレントフォルダにあるファイルを開く
-ExcelApp.Workbooks.Open(fullfile(pwd,'\TestGraphOutput.xlsm')); 
-
-% マクロ：MakingGraphの実行。マクロは"Module1"モジュールに定義され、引数は不要です。 
-retVal = ExcelApp.Run('MakingGraph'); 
-
-% Excelを終了し、オブジェクトを消去します。
-ExcelApp.Quit; 
-ExcelApp.release;
-
-winopen("TestGraphOutput.xlsm");
+% % オブジェクトの生成 
+% ExcelApp = actxserver('Excel.Application'); 
+% % Excel画面の表示 
+% ExcelApp.Visible = 1; 
+% % カレントフォルダにあるファイルを開く
+% ExcelApp.Workbooks.Open(fullfile(pwd,'\TestGraphOutput.xlsm')); 
+% 
+% % マクロ：MakingGraphの実行。マクロは"Module1"モジュールに定義され、引数は不要です。 
+% retVal = ExcelApp.Run('MakingGraph'); 
+% 
+% % Excelを終了し、オブジェクトを消去します。
+% ExcelApp.Quit; 
+% ExcelApp.release;
+% 
+% winopen("TestGraphOutput.xlsm");
 
 
 
